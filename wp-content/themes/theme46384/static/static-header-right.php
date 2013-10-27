950c470f76b529d1aa346d2598f12a169a24d3e5
gomoryt <gomory.tamas@g-b-p.eu>
2013-10-27 19:23:58 +0100

    template header basztatás

---
+++ wp-content/themes/theme46384/static/static-header-right.php
@@ -0,0 +1,7 @@
+		<p style="text-align:right;padding-top:30px; margin-bottom: 0px;">
+		<a href="/hu"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/flag-hu.png"/></a>  <a href="/cr"><img src="http://www.nookofeurope.eu/wp-content/themes/theme46384/images/icons/flag-cr.png"/></a>  <a href="/en"><img src="http://www.nookofeurope.eu/wp-content/themes/theme46384/images/icons/flag-en.png"/></a><br/><br/><br/>
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/green_dots.png"/>
+		</p>
+		<p style="color:#4b4b4d;text-align:right; font-size:14px; font-weight:bold; font-style: italic;">
+			A cross-border region where rivers<br/>connect, not divide
+		</p>
