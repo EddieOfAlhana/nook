7cc5e35f732c590c12b45250a976f7e621c6cd0c
gomoryt <gomory.tamas@g-b-p.eu>
2013-10-27 19:05:31 +0100

    template footer basztatás

--- wp-content/themes/theme46384/wrapper/wrapper-footer.php
+++ wp-content/themes/theme46384/wrapper/wrapper-footer.php
@@ -1,8 +1,8 @@
 <?php /* Wrapper Name: Footer */ ?>
-<div class="span1" style="margin-top:100px;">
+<div class="span1" style="margin-top:100px;margin-right:0px;">
 	<img src="http://www.nookofeurope.eu/wp-content/themes/theme46384/images/icons/flag-eu.png" alt="The project is co-financed by the Euopean Union through the<br> Hungary-Croatia IPA Cross-border Co-operation Programme" title="European Union"/>
 </div>
-<div class="span5" style="margin-top:100px;">
+<div class="span6" style="margin-top:100px;margin-left:0px;">
 	The project is co-financed by the Euopean Union through the<br> Hungary-Croatia IPA Cross-border Co-operation Programme
 </div>
 <div class="span5" style="text-align:right">
