5a0263ea6dbdcec8744ca2f6a42c56bfe2a249ba
gomoryt <gomory.tamas@g-b-p.eu>
2013-10-27 18:49:26 +0100

    template footer basztatás

--- wp-content/themes/theme46384/wrapper/wrapper-footer.php
+++ wp-content/themes/theme46384/wrapper/wrapper-footer.php
@@ -1,6 +1,6 @@
 <?php /* Wrapper Name: Footer */ ?>
 <div class="span6">
-	<span>asdasd<img src="http://www.nookofeurope.eu/wp-content/themes/theme46384/images/icons/flag-eu.png"/></span>
+	<span><img src="http://www.nookofeurope.eu/wp-content/themes/theme46384/images/icons/flag-eu.png"/></span>
 	<span>The project is co-financed by the Euopean Union through the<br> Hungary-Croatia IPA Cross-border Co-operation Programme</span>
 </div>
 <div class="span6" style="text-align:right">
@@ -8,34 +8,34 @@
 </div>
 <!--div class="row footer-widgets">
 	<div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-1">
-		<?php dynamic_sidebar("footer-sidebar-1"); ?>
+		<?php //dynamic_sidebar("footer-sidebar-1"); ?>
 	</div>
 	<div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-2">
-		<?php dynamic_sidebar("footer-sidebar-2"); ?>
+		<?php //dynamic_sidebar("footer-sidebar-2"); ?>
 	</div>
 	<div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-3">
-		<?php dynamic_sidebar("footer-sidebar-3"); ?>
+		<?php //dynamic_sidebar("footer-sidebar-3"); ?>
 	</div>
 	<div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-4">
-		<?php dynamic_sidebar("footer-sidebar-4"); ?>
+		<?php //dynamic_sidebar("footer-sidebar-4"); ?>
 	</div>
 </div>
 <div class="row copyright">
 	<div class="span6">
 		<div data-motopress-type="static" data-motopress-static-file="static/static-footer-logo.php">
-			<?php get_template_part("static/static-footer-logo"); ?>
+			<?php //get_template_part("static/static-footer-logo"); ?>
 		</div>
 		<div class="social-nets-wrapper" data-motopress-type="static" data-motopress-static-file="static/static-social-networks.php">
-			<?php get_template_part("static/static-social-networks"); ?>
+			<?php //get_template_part("static/static-social-networks"); ?>
 		</div>
 		<div data-motopress-type="static" data-motopress-static-file="static/static-footer-text.php">
-			<?php get_template_part("static/static-footer-text"); ?>
+			<?php //get_template_part("static/static-footer-text"); ?>
 		</div>
 	</div>
 	<div class="span6" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-5">
-		<?php dynamic_sidebar("footer-sidebar-5"); ?>
+		<?php //dynamic_sidebar("footer-sidebar-5"); ?>
 	</div>
 	<div class="span12" data-motopress-type="static" data-motopress-static-file="static/static-footer-nav.php">
-		<?php get_template_part("static/static-footer-nav"); ?>
+		<?php //get_template_part("static/static-footer-nav"); ?>
 	</div>
 </div-->
