35ca2f473ad6e69888e7dc25ea6a6d844a299f53
gomoryt <gomory.tamas@g-b-p.eu>
2013-10-27 19:41:44 +0100

    template slider basztatás

--- wp-content/themes/theme46384/page-home.php
+++ wp-content/themes/theme46384/page-home.php
@@ -8,6 +8,13 @@ get_header(); ?>
 <div class="motopress-wrapper content-holder clearfix">
 	<div class="container">
 		<div class="row">
+			<div class="span12">
+				<p style="text-align:center;font-size:22px;color:#4b4b4d; font-weight:bold;">
+					Programme for Tourism Investment Attraction in the Drava Eco-Region<br><br>(PIADER)
+				</p>
+			</div>
+		</div>		
+		<div class="row">
 			<div class="span12" data-motopress-wrapper-file="page-home.php" data-motopress-wrapper-type="content">
 				<div class="row">
 					<div class="span12" data-motopress-type="loop" data-motopress-loop-file="loop/loop-page.php">
