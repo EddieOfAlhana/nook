bc30ffe23047801c9e32f7e469b85695ebf966b1
gomoryt <gomory.tamas@g-b-p.eu>
2013-10-27 19:24:26 +0100

    template header basztatás

--- wp-content/themes/theme46384/wrapper/wrapper-header.php
+++ wp-content/themes/theme46384/wrapper/wrapper-header.php
@@ -1,13 +1,13 @@
 <?php /* Wrapper Name: Header */ ?>
 <div class="row">
-	<div class="span12" data-motopress-type="static" data-motopress-static-file="static/static-logo.php">
-		<?php get_template_part("static/static-logo"); ?>
-	</div>
-</div>
-<div class="row">
 	<div class="span3" data-motopress-type="static" data-motopress-static-file="static/static-logo.php">
 		<?php get_template_part("static/static-logo"); ?>
 	</div>
+	<div class="span9" data-motopress-type="static">
+		<?php get_template_part("static/static-header-right"); ?>
+	</div>
+</div>
+<div class="row">
 	<div class="span9" data-motopress-type="static" data-motopress-static-file="static/static-nav.php">
 		<?php get_template_part("static/static-nav"); ?>
 	</div>
