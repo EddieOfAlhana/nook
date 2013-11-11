3f065d9e465b20ae1c0053481548c44e348ccf47
Gömöry Tamás <root@eddie.localdomain>
2013-11-11 21:50:11 +0000

    Lang selector

--- wp-content/themes/theme46384/static/static-header-right.php
+++ wp-content/themes/theme46384/static/static-header-right.php
@@ -1,7 +1,10 @@
-		<p style="text-align:right;padding-top:30px; margin-bottom: 0px;">
-		<a href="/hu"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/flag-hu.png"/></a>  <a href="/cr"><img src="http://www.nookofeurope.eu/wp-content/themes/theme46384/images/icons/flag-cr.png"/></a>  <a href="/en"><img src="http://www.nookofeurope.eu/wp-content/themes/theme46384/images/icons/flag-en.png"/></a><br/><br/><br/>
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/green_dots.png"/>
-		</p>
-		<p style="color:#4b4b4d;text-align:right; font-size:14px; font-weight:bold; font-style: italic;">
+		<div style="text-align:right;padding-top:10px; margin-bottom: 0px;">
+		<style type="text/css"> .lang_sel_list_horizontal { float:right; }</style>
+		<?php do_action('icl_language_selector'); ?>
+		
+			<!--a href="?lang=en"><img src="/wp-content/plugins/sitepress-multilingual-cms/res/flags/en.png"/></a>  <a href="?lang=hu"><img src="/wp-content/plugins/sitepress-multilingual-cms/res/flags/hu.png"/></a>  <a href="?lang=hr"><img src="/wp-content/plugins/sitepress-multilingual-cms/res/flags/hr.png"/></a--><br/><br/>
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/green_dots.png"/>
+		</div>
+		<div style="color:#4b4b4d;text-align:right; font-size:14px; font-weight:bold; font-style: italic;">
 			A cross-border region where rivers<br/>connect, not divide
-		</p>
+		</div>
